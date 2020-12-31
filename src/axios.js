25512245a16516af5161cdaf46d00c563dd9b39b
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-12-30 17:39:25 -0800

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/online-store-anis/us-central1/api', // THE API (CLOUD F*) URL
+  baseURL: 'https://us-central1-online-store-anis.cloudfunctions.net/api', // THE API (CLOUD F*) URL
 });
 
 export default instance;
+
+//https://us-central1-online-store-anis.cloudfunctions.net/api
+
+// testing
+//http://localhost:5001/online-store-anis/us-central1/api
